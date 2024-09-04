17e0b9a23157c4ac755820f024dddbbb9b00adb9
Konrad <konrad@auterion.com>
2024-09-04 15:56:12 +0200

    ci build_all_target: apply with filter

--- Tools/generate_board_targets_json.py
+++ Tools/generate_board_targets_json.py
@@ -25,9 +25,15 @@ parser.add_argument('-p', '--pretty', dest='pretty', action='store_true',
                     help='Pretty output instead of a single line')
 parser.add_argument('-g', '--groups', dest='group', action='store_true',
                     help='Groups targets')
+parser.add_argument('-f', '--filter', dest='filter', help='comma separated list of board names to use instead of all')
 
 args = parser.parse_args()
 verbose = args.verbose
+
+board_filter = []
+if args.filter:
+    for board in args.filter.split(','):
+        board_filter.append(board)
 
 build_configs = []
 grouped_targets = {}
@@ -129,6 +135,10 @@ for manufacturer in os.scandir(os.path.join(source_dir, 'boards')):
                 label = files.name[:-9]
                 target_name = manufacturer.name + '_' + board.name + '_' + label
 
+                if board_filter and not board_name in board_filter:
+                    if verbose: print(f'excluding board {board_name} ({target_name})')
+                    continue
+
                 if board_name in excluded_boards:
                     if verbose: print(f'excluding board {board_name} ({target_name})')
                     continue
@@ -173,93 +183,72 @@ if (args.group):
     #   nuttx-0
     #   nuttx-1
     final_groups = []
-    temp_group = []
-    group_number = {}
     last_man = ''
     last_arch = ''
     SPLIT_LIMIT = 10
     LOWER_LIMIT = 5
     for arch in grouped_targets:
-        if(last_arch == ''):
-            last_arch = arch
-        if(arch not in group_number):
-                group_number[arch] = 0
-
-        if(last_arch != arch and len(temp_group) > 0):
-            group_name = last_arch + "-" + str(group_number[last_arch])
-            group_number[last_arch] += 1
-            targets = comma_targets(temp_group)
-            final_groups.append({
-                "container": grouped_targets[last_arch]['container'],
-                "targets": targets,
-                "arch": last_arch,
-                "group": group_name,
-                "len": len(temp_group)
-            })
-            last_arch = arch
-            temp_group = []
+        temp_group = []
         for man in grouped_targets[arch]['manufacturers']:
-            for tar in grouped_targets[arch]['manufacturers'][man]:
-                if(last_man != man):
-                    man_len = len(grouped_targets[arch]['manufacturers'][man])
-                    if(man_len > LOWER_LIMIT and man_len < (SPLIT_LIMIT + 1)):
-                        # Manufacturers can have their own group
-                        group_name = arch + "-" + man
-                        targets = comma_targets(grouped_targets[arch]['manufacturers'][man])
-                        last_man = man
-                        final_groups.append({
-                            "container": grouped_targets[arch]['container'],
-                            "targets": targets,
-                            "arch": arch,
-                            "group": group_name,
-                            "len": len(grouped_targets[arch]['manufacturers'][man])
-                        })
-                    elif(man_len >= (SPLIT_LIMIT + 1)):
-                        # Split big man groups into subgroups
-                        # example: Pixhawk
-                        chunk_limit = SPLIT_LIMIT
-                        chunk_counter = 0
-                        for chunk in chunks(grouped_targets[arch]['manufacturers'][man], chunk_limit):
-                            group_name = arch + "-" + man + "-" + str(chunk_counter)
-                            targets = comma_targets(chunk)
-                            last_man = man
-                            final_groups.append({
-                                "container": grouped_targets[arch]['container'],
-                                "targets": targets,
-                                "arch": arch,
-                                "group": group_name,
-                                "len": len(chunk),
-                            })
-                            chunk_counter += 1
-                    else:
-                        temp_group.append(tar)
-
-            if(last_arch != arch and len(temp_group) > 0):
-                group_name = last_arch + "-" + str(group_number[last_arch])
-                group_number[last_arch] += 1
-                targets = comma_targets(temp_group)
-                final_groups.append({
-                    "container": grouped_targets[last_arch]['container'],
-                    "targets": targets,
-                    "arch": last_arch,
-                    "group": group_name,
-                    "len": len(temp_group)
-                })
-                last_arch = arch
-                temp_group = []
-            if(len(temp_group) > (LOWER_LIMIT - 1)):
-                group_name = arch + "-" + str(group_number[arch])
-                last_arch = arch
-                group_number[arch] += 1
-                targets = comma_targets(temp_group)
+            man_len = len(grouped_targets[arch]['manufacturers'][man])
+            if(man_len > LOWER_LIMIT and man_len < (SPLIT_LIMIT + 1)):
+                # Manufacturers can have their own group
+                group_name = arch + "-" + man
+                targets = comma_targets(grouped_targets[arch]['manufacturers'][man])
                 final_groups.append({
                     "container": grouped_targets[arch]['container'],
                     "targets": targets,
                     "arch": arch,
                     "group": group_name,
-                    "len": len(temp_group)
+                    "len": len(grouped_targets[arch]['manufacturers'][man])
                 })
-                temp_group = []
+            elif(man_len >= (SPLIT_LIMIT + 1)):
+                # Split big man groups into subgroups
+                # example: Pixhawk
+                chunk_limit = SPLIT_LIMIT
+                chunk_counter = 0
+                for chunk in chunks(grouped_targets[arch]['manufacturers'][man], chunk_limit):
+                    group_name = arch + "-" + man + "-" + str(chunk_counter)
+                    targets = comma_targets(chunk)
+                    final_groups.append({
+                        "container": grouped_targets[arch]['container'],
+                        "targets": targets,
+                        "arch": arch,
+                        "group": group_name,
+                        "len": len(chunk),
+                    })
+                    chunk_counter += 1
+            else:
+                temp_group.extend(grouped_targets[arch]['manufacturers'][man])
+
+        temp_len = len(temp_group)
+        chunk_counter = 0
+        if(temp_len > 0 and temp_len < (SPLIT_LIMIT + 1)):
+            group_name = arch + "-" + str(chunk_counter)
+            targets = comma_targets(temp_group)
+            final_groups.append({
+                "container": grouped_targets[arch]['container'],
+                "targets": targets,
+                "arch": arch,
+                "group": group_name,
+                "len": temp_len
+            })
+        elif(temp_len >= (SPLIT_LIMIT + 1)):
+            # Split big man groups into subgroups
+            # example: Pixhawk
+            chunk_limit = SPLIT_LIMIT
+            chunk_counter = 0
+            for chunk in chunks(temp_group, chunk_limit):
+                group_name = arch + "-" + str(chunk_counter)
+                targets = comma_targets(chunk)
+                final_groups.append({
+                    "container": grouped_targets[arch]['container'],
+                    "targets": targets,
+                    "arch": arch,
+                    "group": group_name,
+                    "len": len(chunk),
+                })
+                chunk_counter += 1
     if(verbose):
         import pprint
         print("================")
